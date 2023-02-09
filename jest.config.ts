c9f087b8c541fc3b6383eb82404c52003769cc59
Arthur Akira <arthurakira.dev@gmail.com>
2023-02-09 14:18:14 -0300

    chore: setup docker

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+/* eslint-disable no-useless-computed-key */
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const jestConfig: JestConfigWithTsJest = {
+  clearMocks: true,
+  collectCoverage: false,
+  coverageDirectory: "coverage",
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/dist"],
+  transform: {
+    "^.+\\.(t|j)sx?$": "@swc/jest",
+  },
+  moduleNameMapper: {
+    "^@core/(.*)$": "<rootDir>/src/core/$1",
+    "^@infra/(.*)$": "<rootDir>/src/infra/$1",
+    "^@domain/(.*)$": "<rootDir>/src/domain/$1",
+    "^@app/(.*)$": "<rootDir>/src/app/$1",
+    "^@tests/(.*)$": "<rootDir>/tests/$1",
+  },
+};
+
+export default jestConfig;
