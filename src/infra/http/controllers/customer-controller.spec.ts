6b9818c87b4a56e3887f6396f7323ee5acb49580
Arthur Akira <arthurakira.dev@gmail.com>
2023-02-10 14:32:23 -0300

    feat: add list customer method to controller

--- src/infra/http/controllers/customer-controller.spec.ts
+++ src/infra/http/controllers/customer-controller.spec.ts
@@ -1,35 +1,32 @@
-import * as express from "express";
+import httpStatus from "http-status";
 import { CreateCustomerUseCase } from "@app/use-cases/create-customer.use-case";
 import { left, right } from "@core/logic/either";
 import { CustomerFactory } from "@tests/factories/customer-factory";
 import { CustomerErrors } from "@app/errors/customer-errors";
 import { EntityErrors } from "@app/errors/entity-errors";
-import httpStatus from "http-status";
+import { ListCustomerByCPF } from "@app/use-cases/list-customer-by-cpf.use-case";
+import { ExpressFactory } from "@tests/factories/express-factory";
 import { CustomerController } from "./customer-controller";
 import { CreateCustomerRequest } from "../requests/create-customer.request";
+import { ListCustomerRequest } from "../requests/list-customer.request";
 
 describe("Customer Controller", () => {
   let mockCreateCustomer: CreateCustomerUseCase;
+  let mockListCustomer: ListCustomerByCPF;
 
   let sut: CustomerController;
 
-  const createMockResponse = () => {
-    const res = {} as express.Response;
-    res.status = jest.fn().mockReturnValue(res);
-    res.sendStatus = jest.fn().mockReturnValue(res);
-    res.json = jest.fn().mockReturnValue(res);
-    return res;
-  };
-
   beforeEach(() => {
     mockCreateCustomer = { execute: jest.fn() };
-    sut = new CustomerController(mockCreateCustomer);
+    mockListCustomer = { execute: jest.fn() };
+
+    sut = new CustomerController(mockCreateCustomer, mockListCustomer);
   });
 
   describe("Create Customer", () => {
     it("Should return the Customer View Model", async () => {
       const mockCustomerDTO = CustomerFactory.generateCustomerDTO();
-      const mockResponse = createMockResponse();
+      const mockResponse = ExpressFactory.createMockResponse();
       const request = {
         body: {
           name: mockCustomerDTO.name,
@@ -46,7 +43,7 @@ describe("Customer Controller", () => {
     });
 
     it("Should return a Conflict Error if the CPF is already registered", async () => {
-      const mockResponse = createMockResponse();
+      const mockResponse = ExpressFactory.createMockResponse();
       const request = {
         body: {
           name: "Fake Name",
@@ -68,7 +65,7 @@ describe("Customer Controller", () => {
     });
 
     it("Should return a Unprocessable Entity Error if the cpf, name or birthday is invalid", async () => {
-      const mockResponse = createMockResponse();
+      const mockResponse = ExpressFactory.createMockResponse();
       const request = {
         body: {
           name: "Fake Name",
@@ -90,7 +87,7 @@ describe("Customer Controller", () => {
     });
 
     it("Should return an Internal Server Error if an unknown error occurs", async () => {
-      const mockResponse = createMockResponse();
+      const mockResponse = ExpressFactory.createMockResponse();
       const request = {
         body: {
           name: "Fake Name",
@@ -109,6 +106,79 @@ describe("Customer Controller", () => {
         name: "Internal Server Error",
         message: "Unknown Error",
       });
+    });
+  });
+
+  describe("List Customer", () => {
+    it("Should return the Customer View Model", async () => {
+      const mockCustomerDTO = CustomerFactory.generateCustomerDTO();
+      const mockResponse = ExpressFactory.createMockResponse();
+      const request = {
+        params: {
+          customerCPF: mockCustomerDTO.cpf,
+        },
+      } as ListCustomerRequest;
+      jest.spyOn(mockListCustomer, "execute").mockResolvedValueOnce(right(mockCustomerDTO));
+
+      await sut.listCustomerByCPF(request, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(httpStatus.OK);
+      expect(mockResponse.json).toHaveBeenCalledWith(expect.any(Object));
+    });
+
+    it("Should return a Unprocessable Entity Error if the CPF is invalid", async () => {
+      const mockResponse = ExpressFactory.createMockResponse();
+      const request = {
+        params: {
+          customerCPF: "690.141.340-90",
+        },
+      } as ListCustomerRequest;
+      jest
+        .spyOn(mockListCustomer, "execute")
+        .mockResolvedValueOnce(left(new EntityErrors.EntityCreationError("Invalid CPF")));
+
+      await sut.listCustomerByCPF(request, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(httpStatus.UNPROCESSABLE_ENTITY);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        name: "Unprocessable Entity",
+        message: "Invalid CPF",
+      });
+    });
+
+    it("Should return an Internal Server Error if an unknown error occurs", async () => {
+      const mockResponse = ExpressFactory.createMockResponse();
+      const request = {
+        params: {
+          customerCPF: "",
+        },
+      } as ListCustomerRequest;
+      jest
+        .spyOn(mockListCustomer, "execute")
+        .mockResolvedValueOnce(left(new Error("Unknown Error")));
+
+      await sut.listCustomerByCPF(request, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(httpStatus.INTERNAL_SERVER_ERROR);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        name: "Internal Server Error",
+        message: "Unknown Error",
+      });
+    });
+
+    it("Should return a Not Found Error if the customer does not exist", async () => {
+      const mockCustomerDTO = CustomerFactory.generateCustomerDTO();
+      const mockResponse = ExpressFactory.createMockResponse();
+      const request = {
+        params: {
+          customerCPF: mockCustomerDTO.cpf,
+        },
+      } as ListCustomerRequest;
+      jest
+        .spyOn(mockListCustomer, "execute")
+        .mockResolvedValueOnce(left(new CustomerErrors.NotFoundError()));
+
+      await sut.listCustomerByCPF(request, mockResponse);
     });
   });
 });
--- src/infra/http/controllers/customer-controller.ts
+++ src/infra/http/controllers/customer-controller.ts
@@ -4,16 +4,24 @@ import { EntityErrors } from "@app/errors/entity-errors";
 import { CreateCustomerUseCase } from "@app/use-cases/create-customer.use-case";
 import { BaseController } from "@core/infra/http/base-controller";
 import { CustomerDTO } from "@app/dtos/customer.dto";
+import { ListCustomerByCPF } from "@app/use-cases/list-customer-by-cpf.use-case";
 import { CreateCustomerRequest } from "../requests/create-customer.request";
 import { CustomerViewModel } from "../view-models/customer.view-model";
 import { CustomerPresenter } from "../presenters/customer-presenter";
+import { ListCustomerRequest } from "../requests/list-customer.request";
 
 export class CustomerController extends BaseController {
   private readonly _createCustomerUseCase: CreateCustomerUseCase;
 
-  constructor(createCustomerUseCase: CreateCustomerUseCase) {
+  private readonly _listCustomerByCPFUseCase: ListCustomerByCPF;
+
+  constructor(
+    createCustomerUseCase: CreateCustomerUseCase,
+    listCustomerByCPFUseCase: ListCustomerByCPF
+  ) {
     super();
     this._createCustomerUseCase = createCustomerUseCase;
+    this._listCustomerByCPFUseCase = listCustomerByCPFUseCase;
 
     this.createCustomer = this.createCustomer.bind(this);
   }
@@ -47,5 +55,33 @@ export class CustomerController extends BaseController {
     const customerViewModel: CustomerViewModel = CustomerPresenter.toViewModel(customerDTO);
 
     return this.created<CustomerViewModel>(res, customerViewModel);
+  }
+
+  public async listCustomerByCPF(req: ListCustomerRequest, res: Response): Promise<Response> {
+    const { customerCPF } = req.params;
+
+    const listCustomerOrError = await this._listCustomerByCPFUseCase.execute({
+      cpf: customerCPF,
+    });
+
+    if (listCustomerOrError.isLeft()) {
+      const listCustomerError = listCustomerOrError.result;
+      const errorMessage = listCustomerError.message;
+
+      switch (listCustomerError.constructor) {
+        case EntityErrors.EntityCreationError:
+          return this.unprocessableEntity(res, errorMessage);
+
+        case CustomerErrors.NotFoundError:
+          return this.notFound(res, errorMessage);
+
+        default:
+          return this.fail(res, errorMessage);
+      }
+    }
+
+    const customerDTO: CustomerDTO = listCustomerOrError.result;
+
+    return this.ok<CustomerViewModel>(res, CustomerPresenter.toViewModel(customerDTO));
   }
 }
